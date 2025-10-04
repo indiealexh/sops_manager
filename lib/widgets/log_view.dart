4d04e6f7599e22e52e026977975e043dc56fdea0
Alexander Haslam <aihaslam@ucdavis.edu>
2025-10-03 22:59:48 -0700

    Add `devtools_options.yaml` and clean up `LogView`
    - Introduce `devtools_options.yaml` to store DevTools settings.
    - Remove unnecessary `Spacer` widget in `LogView`.

--- lib/widgets/log_view.dart
+++ lib/widgets/log_view.dart
@@ -93,7 +93,6 @@ class _LogViewState extends State<LogView> {
               selected: _autoScroll,
               onSelected: (v) => setState(() => _autoScroll = v),
             ),
-            const Spacer(),
             TextButton.icon(
               onPressed: widget.onClear,
               icon: const Icon(Icons.clear),
